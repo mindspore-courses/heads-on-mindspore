dcb4ccc92b7d306a29f4197fff343f215664b73f
Xiao <2085127827@qq.com>
2023-07-17 22:16:06 +0800

    测试

--- 3-neural_style/PackedVGG.py
+++ 3-neural_style/PackedVGG.py
@@ -1,20 +1,22 @@
 # coding:utf8
-import torch
-import torch.nn as nn
-from torchvision.models import vgg16
 from collections import namedtuple
 
 import mindspore as ms 
+import mindspore.nn as nn
+from mindcv.models import create_model
 
-class Vgg16(torch.nn.Module):
+#获取每个激活函数后的输出值
+
+class Vgg16(nn.Cell):
     def __init__(self):
         super(Vgg16, self).__init__()
-        features = list(vgg16(pretrained=True).features)[:23]
+
+        features = list(create_model(model_name='vgg16', pretrained=True).features)[:23]#前23
         # the 3rd, 8th, 15th and 22nd layer of \ 
         # self.features are: relu1_2,relu2_2,relu3_3,relu4_3
-        self.features = nn.ModuleList(features).eval()
+        self.features = nn.CellList(features).eval()
 
-    def forward(self, x):
+    def construct(self, x):
         results = []
         for ii, model in enumerate(self.features):
             x = model(x)
