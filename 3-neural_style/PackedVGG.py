6c7edf7e96c2738ea9bc1be5cef50ef1bd33e456
Xiao <2085127827@qq.com>
2023-07-18 21:32:36 +0800

    test

--- 3-neural_style/PackedVGG.py
+++ 3-neural_style/PackedVGG.py
@@ -1,15 +1,12 @@
 # coding:utf8
 from collections import namedtuple
-
 import mindspore.nn as nn
 from mindcv.models import create_model
 
 #获取每个激活函数后的输出值
-
 class Vgg16(nn.Cell):
     def __init__(self):
         super(Vgg16, self).__init__()
-
         features = list(create_model(model_name='vgg16', pretrained=True).features)[:23]#前23
         # the 3rd, 8th, 15th and 22nd layer of \ 
         # self.features are: relu1_2,relu2_2,relu3_3,relu4_3
@@ -21,6 +18,5 @@ class Vgg16(nn.Cell):
             x = model(x)
             if ii in {3, 8, 15, 22}:
                 results.append(x)
-
         vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
         return vgg_outputs(*results)
